fc7746586c8823c63a6af7b7a502010240f26509
Berhane-Hiwet La Rose <berhane.larose@gmail.com>
2020-09-27 02:41:47 -0400

    Create enums, and User and Token types

--- src/typeDefs.js
+++ src/typeDefs.js
@@ -1,7 +1,52 @@
 import { gql } from 'apollo-server'
 
 export default gql`
+    enum UserRole{
+        STUDENT
+        ADMIN
+        MANAGER
+        COORDINATOR
+        GUEST
+    }
+
+    enum Faculty{
+        HUMANITIES
+        MATHEMATICS
+        COMPSCI
+        ADMIN
+    }
+
+    enum YearStatus{
+        CLOSED
+        OPEN
+    }
+
+    type Token{
+        token: String!
+    }
+
+    type User{
+        _id: ID
+        firstName: String
+        lastName: String
+        username: String!
+        email: String!
+        password: String!
+        avatar: String
+        role: UserRole!
+        faculty: Faculty!
+    }
+
     type Query {
         hello: String!
+    }
+    type Mutation {
+        createUser(
+            username: String!,
+            email: String!,
+            password: String!,
+            role: String!,
+            faculty: String!
+        ): Token
     }
 `
